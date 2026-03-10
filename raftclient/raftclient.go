cc71cf03b51b572ff82022c8fbe165d169a44225
kr9sis <mkmw464l@anonaddy.com>
2026-03-10 16:53:38 +0000

    Implemented basic client

--- raftclient/raftclient.go
+++ raftclient/raftclient.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"log"
+	"net"
+)
+
+func main() {
+	addr_str := "[::1]:3007"
+	addr, err := net.ResolveUDPAddr("udp", addr_str)
+	if err != nil {
+		log.Fatalf("Could not resolve %s to UDP address\n", addr_str)
+	}
+	conn, err := net.DialUDP("udp", nil, addr)
+	if err != nil {
+		log.Fatalf("Could not dial %v to UDP address\n", addr)
+	}
+	defer conn.Close()
+	conn.Write([]byte("This is a test\n"))
+}
